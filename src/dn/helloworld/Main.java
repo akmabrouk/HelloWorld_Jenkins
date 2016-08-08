d7842e8bf640b6a694f1422e8a377f1a66d533cf
sunbeltdn <sunbeltdn@163.com>
2016-08-08 17:46:58 +0800

    Initial commit

---
+++ src/dn/helloworld/Main.java
@@ -0,0 +1,9 @@
+package dn.helloworld;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    System.out.println("Hello World, try jenkins...");
+    }
+}
